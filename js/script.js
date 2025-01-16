463dd9279852d4006bf52ae9d879ba6b42733bbf
Camilo Gomez <camigomezbernal@gmail.com>
2025-01-16 15:54:27 -0500

    SE generan vistas y mejora

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const ButtonMasivas = document.getElementById('ButtonMasivas');  // Asegúrate de que el ID sea correcto
     const ButtonPersonales = document.getElementById('ButtonPersonales');  // Asegúrate de que el ID sea correcto
     const ButtonPostnomina = document.getElementById('ButtonPostnomina');  // Asegúrate de que el ID sea correcto
+    const goHomeButton = document.getElementById("goHomeButton");
+    const toggleButton = document.getElementById("toggleBubble");
+    const chatBubble = document.getElementById("chatBubble");
+    const toggleIcon = document.getElementById("toggleIcon");
 
     // Abrir el chat cuando se presiona el botón
     chatbotButton.addEventListener('click', () => {
@@ -34,4 +38,15 @@ document.addEventListener('DOMContentLoaded', function () {
             window.location.href = "postnomina.html";
         });
     }
+
+        // Redirigir al inicio (index.html) cuando se haga clic en el botón INICIO
+    document.getElementById("goHomeButton").addEventListener("click", function() {
+        window.location.href = "index.html"; // Redirige a la página de inicio
+    });
+
+    // Función para alternar el estado de la burbuja (expandir/contraer)
+    toggleButton.addEventListener("click", function() {
+        chatBubble.classList.toggle("collapsed"); // Alterna la clase 'collapsed' en la burbuja
+    });
+    
 });
